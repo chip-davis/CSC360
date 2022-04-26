91156c7f49026426aadd246d4d3bd1c9b30c7dba
chip-davis <chipydavis99@gmail.com>
2022-04-25 19:24:18 -0500

    added create post functionality.

--- hw2/blog/src/App.js
+++ hw2/blog/src/App.js
@@ -5,15 +5,16 @@ import CreatePost from "./CreatePost"
 function App() {
 
   const [user, setUser] = useState("")
-  const posts = [{ title: "A blog post 1", content: "Blog content 1", author: "Chip 1" }, {
-    title: "A blog post 2", content: "Blog content 2", author: "Chip"
-  }, { title: "A blog post 3", content: "Blog content 3", author: "Chip" }]
+  // const posts = [{ title: "A blog post 1", content: "Blog content 1", author: "Chip 1" }, {
+  //   title: "A blog post 2", content: "Blog content 2", author: "Chip"
+  // }, { title: "A blog post 3", content: "Blog content 3", author: "Chip" }]
 
+  const [posts, setPosts] = useState([])
 
   return (
     <div>
       <UserBar user={user} setUser={setUser} />
-      {user && <CreatePost user={user} />}
+      {user && <CreatePost user={user} setPosts={setPosts} posts={posts} />}
       <PostList posts={posts} />
     </div>
   );
--- hw2/blog/src/CreatePost.js
+++ hw2/blog/src/CreatePost.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function CreatePost({ user }) {
+export default function CreatePost({ user, setPosts, posts }) {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  function handleTitle(evt) { setTitle(evt.target.value) }
+  function handleContent(evt) { setContent(evt.target.value) }
+  function handleCreate() {
+    const newPost = { title, content, author: user }
+    setPosts([newPost, ...posts])
+  }
+
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <>
+      <h2> Create a New Post</h2>
+      <form onSubmit={(e) => { e.preventDefault(); handleCreate(e) }}>
 
-      <div>
-        Author: <b>{user}</b>
-      </div>
+        <div>
+          Author: <b>{user}</b>
+        </div>
 
-      <div>
-        <label htmlFor="create-title">Title:</label>
-        <input type="text" name="create-title" id="create-title" />
-      </div>
+        <div>
+          <label htmlFor="create-title">Title:</label>
+          <input type="text" name="create-title" id="create-title" value={title} onChange={handleTitle} />
+        </div>
 
-      <textarea />
-      <input type="submit" value="Create" />
-    </form>
+        <textarea value={content} onChange={handleContent} />
+        <input type="submit" value="Create" />
+      </form>
+    </>
   );
 }
