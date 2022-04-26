93b011d58020472610ec76a61739fecad23f6219
chip-davis <chipydavis99@gmail.com>
2022-04-25 19:00:54 -0500

    moved setUser to App.js to have it be global. implemented logic to only render create post if user is logged in

--- hw2/blog/src/App.js
+++ hw2/blog/src/App.js
@@ -4,6 +4,7 @@ import PostList from "./PostList"
 import CreatePost from "./CreatePost"
 function App() {
 
+  const [user, setUser] = useState("")
   const posts = [{ title: "A blog post 1", content: "Blog content 1", author: "Chip 1" }, {
     title: "A blog post 2", content: "Blog content 2", author: "Chip"
   }, { title: "A blog post 3", content: "Blog content 3", author: "Chip" }]
@@ -11,9 +12,9 @@ function App() {
 
   return (
     <div>
-      <UserBar />
+      <UserBar user={user} setUser={setUser} />
+      {user && <CreatePost user={user} />}
       <PostList posts={posts} />
-      <CreatePost user="Chip!" />
     </div>
   );
 }
